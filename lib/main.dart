ad2c3922cb2133f6d4dfae7cea076cba19819886
Danila Pakhomov <dph.develop@gmail.com>
2022-04-27 21:38:50 +0400

    Added PRO ver. of homework №1

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ abstract class FilmCard {
   final String releaseDate;
   final String description;
   final String language;
+
+  void aboutFilm();
 }
 
 class Film extends FilmCard with LanguageConverter {
@@ -22,6 +24,68 @@ class Film extends FilmCard with LanguageConverter {
     required String language,
   }) : super(id, title, picture, voteAverage, releaseDate, description,
             language);
+
+  @override
+  void aboutFilm() {
+    var languageType = getLanguage(language);
+    print('\nНазвание: $title,'
+        '\nРейтинг: $voteAverage,'
+        '\nДата выхода: $releaseDate,'
+        '\nОписание: $description,'
+        '\nЯзык фильма: ${languageType.toPrettyString()}');
+  }
+}
+
+Future<List<Film>> getFilmList() async {
+  List<Film> films = _getFilms();
+  for (final film in films) {
+    await Future.delayed(const Duration(seconds: 1));
+    film.aboutFilm();
+  }
+  List<Film> filterFilms = _filterFilm(films);
+  print('\nРейтинг фильма больше 9: ');
+  for (final film in filterFilms) {
+    film.aboutFilm();
+  }
+  return films;
+}
+
+List<Film> _filterFilm(List<Film> films) {
+  return films.where((element) => element.voteAverage > 9).toList();
+}
+
+Future<void> main() async {
+  print('Список фильмов: ');
+  await getFilmList();
+}
+
+List<Film> _getFilms() {
+  return [
+    const Film(
+        id: '0',
+        title: 'Зелёная миля',
+        picture: 'img',
+        voteAverage: 9.1,
+        releaseDate: '2000-03-03',
+        description: 'Обвиненный в страшном преступлении, Джон Коффи...',
+        language: 'english'),
+    const Film(
+        id: '1',
+        title: 'Побег из Шоушенка',
+        picture: 'img',
+        voteAverage: 8.9,
+        releaseDate: '1995-02-17',
+        description: 'Бухгалтер Энди Дюфрейн обвинён в убийстве...',
+        language: 'russian'),
+    const Film(
+        id: '2',
+        title: 'Список Шиндлера',
+        picture: 'img',
+        voteAverage: 8.8,
+        releaseDate: '1994-02-18',
+        description: 'Лента рассказывает реальную историю...',
+        language: 'another')
+  ];
 }
 
 enum Language { english, russian, another }
